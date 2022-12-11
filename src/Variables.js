9a08aee37846d6c69b3990b8bc68da9558b7457f
Nakami Hope-Felix <hopefelixn@gmail.com>
2022-12-11 14:03:58 -0500

    Progress on populating main menu Buttons

---
+++ src/Variables.js
@@ -0,0 +1,224 @@
+//Array of objects containing each main page item along with its properties
+export const menuItems = [
+  {
+    1: {
+      Name: "Shack Burger",
+      Color: "Light Green",
+      Mods: ["Single", "Double", "Triple"],
+      Ingredients: [
+        "Bun",
+        "Butter",
+        "Lettuce",
+        "Tomato",
+        "Shack Sauce",
+        "Onions",
+        "Cheese",
+        "Burger Patty",
+      ],
+    },
+  },
+  {
+    2: {
+      Name: "Cheese Burger",
+      Color: "Light Green",
+      Mods: ["Single", "Double", "Triple"],
+      Ingredients: ["Bun", "Butter", "Cheese", "Burger Patty"],
+    },
+  },
+  {
+    3: {
+      Name: "Smoke Shake",
+      Color: "Light Green",
+      Mods: ["Single", "Double", "Triple"],
+      Ingredients: [
+        "Bun",
+        "Butter",
+        "Cheese",
+        "Shake Sauce",
+        "Cherry Peppers",
+        "Burger Patty",
+      ],
+    },
+  },
+  {
+    4: {
+      Name: "Shroom Burger",
+      Color: "Light Green",
+      Mods: false,
+      Ingredients: [
+        "Bun",
+        "Butter",
+        "Cheese",
+        "Shake Sauce",
+        "Cherry Peppers",
+        "Shroom Patty",
+      ],
+    },
+  },
+  {
+    5: {
+      Name: "Shack Stack",
+      Color: "Light Green",
+      Mods: false,
+      Ingredients: [
+        "Bun",
+        "Butter",
+        "Lettuce",
+        "Tomato",
+        "Shack Sauce",
+        "Onions",
+        "Cheese",
+        "Burger Patty",
+        "Shroom Patty",
+      ],
+    },
+  },
+  {
+    6: {
+      Name: "Bacon Avocado",
+      Color: "Light Green",
+      Mods: false,
+      Ingredients: ["Bun", "Butter", "Burger Patty", "Bacon", "Avacado"],
+    },
+  },
+  {
+    7: {
+      Name: "Bacon Cheese Burger",
+      Color: "Light Green",
+      Mods: false,
+      Ingredients: ["Bun", "Butter", "Burger Patty", "Bacon", "Cheese"],
+    },
+  },
+  {
+    8: {
+      Name: "Burger Mods",
+      Color: "Light Green",
+      Mods: "Burger Mod Menu",
+      Ingredients: false,
+    },
+  },
+  {
+    9: {
+      Name: "Fries",
+      Color: "Light Green",
+      Mods: false,
+      Ingredients: ["Fries"],
+    },
+  },
+  {
+    10: {
+      Name: "Fry Mod",
+      Color: "Teal",
+      Mods: "Fry Mod Menu",
+      Ingredients: false,
+    },
+  },
+  {
+    11: {
+      Name: "Hot Dog",
+      Color: "Light Blue",
+      Mods: false,
+      Ingredients: ["Hot Dog Bun", "Hot Dog"],
+    },
+  },
+  {
+    12: {
+      Name: "Dog Mods",
+      Color: "Light Blue",
+      Mods: "Dog Mod Menu",
+      Ingredients: false,
+    },
+  },
+  {
+    13: {
+      Name: "Dog Mods",
+      Color: "Light Blue",
+      Mods: "Dog Mod Menu",
+      Ingredients: false,
+    },
+  },
+  {
+    14: {
+      Name: "$$ Cheese Sauce",
+      Color: "Tiffany Blue",
+      Mods: false,
+      Ingredients: ["Cheese Sauce"],
+    },
+  },
+  {
+    15: {
+      Name: "Side of Cheese Sauce FRIES",
+      Color: "Tiffany Blue",
+      Mods: false,
+      Ingredients: ["Hot Dog Bun", "Hot Dog"],
+    },
+  },
+  {
+    16: {
+      Name: "$$ Avocado",
+      Color: "Tiffany Blue",
+      Mods: false,
+      Ingredients: ["Avocado"],
+    },
+  },
+  {
+    17: {
+      Name: "$$ Bacon",
+      Color: "Tiffany Blue",
+      Mods: false,
+      Ingredients: ["Bacon"],
+    },
+  },
+  {
+    18: {
+      Name: "Chicken Shack",
+      Color: "Yellow",
+      Mods: false,
+      Ingredients: [
+        "Herb Mayo",
+        "Chicken Patty",
+        "Pickle Chips",
+        "Butter",
+        "Bun",
+      ],
+    },
+  },
+  {
+    19: {
+      Name: "Chicken Mods",
+      Color: "Yellow",
+      Mods: "Chicken Mod Menu",
+      Ingredients: [
+        "Herb Mayo",
+        "Chicken Patty",
+        "Pickle Chips",
+        "Butter",
+        "Bun",
+      ],
+    },
+  },
+  {
+    20: {
+      Name: "Bites 10 Pc",
+      Color: "Yellow",
+      Mods: "Chicken Bites Menu",
+      Ingredients: ["Chicken Nuggets"],
+    },
+  },
+  {
+    21: {
+      Name: "Bites 6 Pc",
+      Color: "Yellow",
+      Mods: "Chicken Bites Menu",
+      Ingredients: ["Chicken Nuggets"],
+    },
+  },
+  {
+    22: {
+      Name: "Bites Mod Menu",
+      Color: "Yellow",
+      Mods: "Chicken Bites Menu",
+      Ingredients: false,
+    },
+  },
+];
